e121abad7793e73217a9b29630110656329c5344
valentinmadiot <valentin.madiot@gmail.com>
2025-06-18 01:46:40 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,14 @@ const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
   navClose = document.getElementById("nav-close");
 
-/* Menu show */
+/* Menu Show */
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
   });
 }
 
-/* Menu hidden */
+/* Menu Hidden */
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
